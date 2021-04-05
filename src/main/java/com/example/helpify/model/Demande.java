caf505df12c3e075db9996010e671f3489514261
unknown <Harrouchabdou@gmail.com>
2021-04-05 06:00:46 +0100

    harchi

--- src/main/java/com/example/helpify/model/Demande.java
+++ src/main/java/com/example/helpify/model/Demande.java
@@ -8,5 +8,75 @@ public class Demande {
     private String adresse;
     private String etat;
 
+    public Demande() {
+    }
 
+    public Demande(long id, String titre, String categorie, String description, String adresse, String etat) {
+        this.id = id;
+        this.titre = titre;
+        this.categorie = categorie;
+        this.description = description;
+        this.adresse = adresse;
+        this.etat = etat;
+    }
+
+    @Override
+    public String toString() {
+        return "Demande{" +
+                "id=" + id +
+                ", titre='" + titre + '\'' +
+                ", categorie='" + categorie + '\'' +
+                ", description='" + description + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", etat='" + etat + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getEtat() {
+        return etat;
+    }
+
+    public void setEtat(String etat) {
+        this.etat = etat;
+    }
 }
---
+++ src/main/java/com/example/helpify/service/GestionDemandeService.java
@@ -0,0 +1,39 @@
+package com.example.helpify.service;
+
+import com.example.helpify.model.Demande;
+import com.example.helpify.model.User;
+import com.example.helpify.repository.DemandeRepository;
+
+
+public class GestionDemandeService {
+    public static boolean ajouterDemande(Demande demande, DemandeRepository demandeRepository) {
+        if (demande == null) return false;
+        demandeRepository.save(demande);
+        return true;
+    }
+
+    public static boolean modifierDemande(Demande demande, DemandeRepository demandeRepository) {
+        Demande foundDemande;
+        foundDemande = demandeRepository.findDemandeById(demande.getId());
+
+        if (foundDemande == null) return false;
+        foundDemande.setAdresse(demande.getAdresse());
+        foundDemande.setCategorie(demande.getCategorie());
+        foundDemande.setDescription(demande.getDescription());
+        foundDemande.setEtat(demande.getEtat());
+        foundDemande.setTitre(demande.getTitre());
+        demandeRepository.save(foundDemande);
+        return true;
+    }
+
+    public static boolean supprimerDemande(long id, DemandeRepository demandeRepository) {
+        Demande foundDemande;
+        foundDemande = demandeRepository.findDemandeById(id);
+
+        if (foundDemande == null) return false;
+
+
+        demandeRepository.delete(foundDemande);
+        return true;
+    }
+}
