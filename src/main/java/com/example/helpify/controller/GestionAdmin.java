9c4e1a2e3a2e2b787ac4a6a6c126d4a748dc1d09
OnceLearner <redaamir972@gmail.com>
2021-06-04 18:07:23 +0100

    adding rest and cros origin anotation

--- src/main/java/com/example/helpify/controller/GestionAdmin.java
+++ src/main/java/com/example/helpify/controller/GestionAdmin.java
@@ -4,14 +4,13 @@ import com.example.helpify.model.Admin;
 import com.example.helpify.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@RestController
+@CrossOrigin
 public class GestionAdmin {
 
     @Autowired
